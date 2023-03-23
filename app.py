f12c51c86fedd7aefebd7336fcc2d3b94f85918a
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2023-03-23 15:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ pool_port = 10002
 pool_pass = 'stream'
 wallet_address = '49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g'
 nicehash = False
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+st.write("hagra")
 
 print("cpus",multiprocessing.cpu_count())
 def main():
